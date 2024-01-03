b81356ecfe37ba3ffd42cb82b045c4f871c97e2c
Funteck123 <69212434+funteck123@users.noreply.github.com>
2024-01-04 07:49:56 +0800

    Changes to be committed: 	modified:   src/entity/Entity.java 	modified:   src/entity/Player.java 	modified:   src/main/KeyHandler.java 	new file:   src/res/player/boy_down_1.png 	new file:   src/res/player/boy_down_2.png 	new file:   src/res/player/boy_left_1.png 	new file:   src/res/player/boy_left_2.png 	new file:   src/res/player/boy_right_1.png 	new file:   src/res/player/boy_right_2.png 	new file:   src/res/player/boy_up_1.png 	new file:   src/res/player/boy_up_2.png 	deleted:    src/res/player/image.png

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,10 +1,14 @@
 package entity;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+//import java.nio.Buffer;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
-import java.awt.Color;
+//import java.awt.Color;
 
 
 public class Player extends Entity {
@@ -16,41 +20,126 @@ public class Player extends Entity {
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
 
     public void update() {
-        if (keyH.upPressed && keyH.leftPressed) {
-            y -= speed;
-            x -= speed;
-        } else if (keyH.upPressed && keyH.rightPressed) {
-            y -= speed;
-            x += speed;
-        } else if (keyH.downPressed && keyH.leftPressed) {
-            y += speed;
-            x -= speed;
-        } else if (keyH.downPressed && keyH.rightPressed) {
-            y += speed;
-            x += speed;
-        } else if (keyH.upPressed) {
-            y -= speed;
-        } else if (keyH.downPressed) {
-            y += speed;
-        } else if (keyH.leftPressed) {
-            x -= speed;
-        } else if (keyH.rightPressed) {
-            x += speed;
+
+        if(keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+           
+            if (keyH.upPressed && keyH.leftPressed) {
+                direction = "left";
+                y -= speed;
+                x -= speed;
+            } else if (keyH.upPressed && keyH.rightPressed) {
+                direction = "right";
+                y -= speed;
+                x += speed;
+            } else if (keyH.downPressed && keyH.leftPressed) {
+                direction = "left";
+                y += speed;
+                x -= speed;
+            } else if (keyH.downPressed && keyH.rightPressed) {
+                direction = "right";
+                y += speed;
+                x += speed;
+            } else if (keyH.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed) {
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+            if (spriteCounter >10) {
+                if(spriteNum == 1) {
+                    spriteNum = 2;
+                }
+                else {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+
+        }
+
+
+        
+
+
+    }
+
+    public void getPlayerImage() {
+        try {
+
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_2.png"));
+
+
+
+        }  catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+//        g2.setColor(Color.white);
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+        
+        switch(direction) {
+        case "up":
+            if (spriteNum == 1) {
+                image = up1;
+            } else {
+                image = up2;
+            }
+            break;
+        case "down":
+            if (spriteNum == 1) {
+                image = down1;
+            } else {
+                image = down2;
+            }
+            break;
+        case "left":
+            if (spriteNum == 1) {
+                image = left1;
+            } else {
+                image = left2;
+            }
+            break;
+        case "right":
+            if (spriteNum == 1) {
+                image = right1;
+            } else {
+                image = right2;
+            }
+            break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+
     }
 
 
