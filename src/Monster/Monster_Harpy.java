950558c1a9c7afc67b37f2013781f136c106d998
badrfaez1 <22114471siswa.um.edu.my>
2024-01-13 20:50:11 +0800

    Changes to be committed:

---
+++ src/Monster/Monster_Harpy.java
@@ -0,0 +1,51 @@
+package Monster;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class Monster_Harpy extends Entity {
+    public Monster_Harpy(GamePanel gp) {
+        super(gp);
+        String name="Harpy";
+        maxLife=4;
+        life=maxLife;
+     solidArea.x=3;
+     solidArea.y=10;
+     solidArea.width=42;
+     solidArea.height=30;
+     solidAreaDefaultX=solidArea.x;
+     solidAreaDefaultY=solidArea.y;
+        direction = "down";
+        speed = 1;
+        getImage();
+        // setDialogue();
+    }
+    
+    public void getImage() {
+        up1=setup("/res/Monster/Harpy_up1");
+        up2=setup("/res/Monster/Harpy_up2");
+        down1=setup("/res/Monster/Harpy_down1");
+        down2=setup("/res/Monster/Harpy_down2");
+        left1=setup("/res/Monster/Harpy_left1");
+        left2=setup("/res/Monster/Harpy_left2");
+        right1=setup("/res/Monster/Harpy_right1");
+        right2=setup("/res/Monster/Harpy_right2");
+    }
+
+    // public void setDialogue() {
+
+    //     dialogues[0] = "Sword's name: 'Banterbringer.'\nReady for a demo?";
+    //     dialogues[1] = "You have encountered a warrior.\n\n1. Talk\n2. Fight\n3. Run";
+    //     dialogues[2] = "I have neat sword.";
+        
+    // }
+    
+    public void setAction() {
+        super.setAction();    
+    }
+
+    public void speak() {
+        super.speak();
+    }
+
+}
